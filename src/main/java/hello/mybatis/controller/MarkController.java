c463fc9a0d692d270a3e49462bb694e25fd2ecd7
KatsuraKotarou <feilong80an@gmail.com>
2015-01-22 17:01:18 +0800

    add log output

--- src/main/java/hello/mybatis/controller/MarkController.java
+++ src/main/java/hello/mybatis/controller/MarkController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import hello.mybatis.data.MarkMapper;
 import hello.mybatis.model.Mark;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +18,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/mark")
 public class MarkController {
+	private static Log log = LogFactory.getLog(MarkController.class);
+	
 	@Autowired
 	private MarkMapper markMapper;
 	@RequestMapping("/list")
 	public ModelAndView listAll(HttpServletRequest request, HttpServletResponse response) {
-		String a = "a";
-		
 		List<Mark> marks = markMapper.getItemMarks(1);
+		log.info(marks.size());
 		ModelAndView mav = new ModelAndView("list");
 		mav.addObject("marks", marks);
 		return mav;
