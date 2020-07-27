ab39d55c2e8ca9972c71133ed1089c7a36b9a5ea
spike001984 <panfeilong@hotmail.com>
2020-07-27 18:04:26 +0800

    Springboot

---
+++ src/main/java/com/tarpitout/springboot/AioApplication.java
@@ -0,0 +1,15 @@
+package com.tarpitout.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.tarpitout.springboot.controller"})
+public class AioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AioApplication.class, args);
+	}
+
+}
--- src/test/java/com/github/katsurakkkk/concurrencyinpratice/ConcurrencyInPraticeTest.java
+++ src/test/java/com/github/katsurakkkk/concurrencyinpratice/ConcurrencyInPraticeTest.java
@@ -1,6 +1,7 @@
 package com.github.katsurakkkk.concurrencyinpratice;
 
 import org.junit.*;
+import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.*;
 
